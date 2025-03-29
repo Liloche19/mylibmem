8916d47585a3065dcf2145aa546bd32828f2a2bf
Liloche19 <lilian.locher@epitech.eu>
2025-03-29 17:44:24 +0100

    [+] my_exit function added

---
+++ lib/mem/my_exit.c
@@ -0,0 +1,28 @@
+#include "../../include/mem.h"
+
+int my_string_len(char *str)
+{
+    int i = 0;
+
+    if (str == NULL)
+        return 0;
+    while (str[i] != '\0'){
+        i++;
+    }
+    return i;
+}
+
+void my_exit(char *message, int code)
+{
+    int size = my_string_len(message);
+
+    if (code != SUCCESS_CODE){
+        write(2, message, size);
+        free_all();
+        exit(code);
+    }
+    write(1, message, size);
+    free_all();
+    exit(code);
+    return;
+}
